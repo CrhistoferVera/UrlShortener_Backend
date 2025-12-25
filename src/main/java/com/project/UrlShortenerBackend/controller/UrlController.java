4d3749c1d54d72a16a740326aa92b06922e2f865
CrhistoferVera <cristofervera110@gmail.com>
2025-12-25 01:33:37 -0400

    Add the url controller

--- src/main/java/com/project/UrlShortenerBackend/controller/UrlController.java
+++ src/main/java/com/project/UrlShortenerBackend/controller/UrlController.java
@@ -1,7 +1,35 @@
 package com.project.UrlShortenerBackend.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.project.UrlShortenerBackend.dto.CreateUrlDTO;
+import com.project.UrlShortenerBackend.dto.ResponseUrlDTO;
+import com.project.UrlShortenerBackend.model.ShortUrl;
+import com.project.UrlShortenerBackend.service.UrlService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+
+@RequiredArgsConstructor
 @RestController
+ @RequestMapping("/api/urls")
 public class UrlController {
+    private final UrlService urlService;
+
+    @PostMapping
+    public ResponseUrlDTO create(@RequestBody CreateUrlDTO createUrlDTO){
+        ShortUrl shortUrl= urlService.createShortUrl(createUrlDTO.getOriginalUrl());
+        return new ResponseUrlDTO("http://localhost:8080/" + shortUrl.getShortUrl(),0L);
+    }
+    @GetMapping("/{code}")
+    public ResponseEntity<Void> redirect(@PathVariable String code){
+        ShortUrl url = urlService.getByCode(code);
+        url.setAccessCount(url.getAccessCount()+1);
+        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(url.getOriginalUrl())).build();
+    }
+
+
+
+
 }
