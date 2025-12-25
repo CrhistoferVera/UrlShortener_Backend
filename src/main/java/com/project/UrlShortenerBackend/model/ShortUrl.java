629d3d7d0db68f438f1fee3c13d391cc11bdf18b
CrhistoferVera <cristofervera110@gmail.com>
2025-12-25 00:52:22 -0400

    the first commit with the structure of the backend

---
+++ src/main/java/com/project/UrlShortenerBackend/model/ShortUrl.java
@@ -0,0 +1,23 @@
+package com.project.UrlShortenerBackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "short_urls")
+public class ShortUrl {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 2048)
+    private String originalUrl;
+    @Column(nullable = false, unique = true, length = 10)
+    private String shortUrl;
+    private LocalDateTime createdAt;
+    private Long accessAccount = 0L;
+}
---
+++ src/main/java/com/project/UrlShortenerBackend/service/UrlService.java
@@ -0,0 +1,4 @@
+package com.project.UrlShortenerBackend.service;
+
+public class UrlService {
+}
